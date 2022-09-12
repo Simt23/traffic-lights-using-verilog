47571a1d610fc65cba0fcc9be4da743d24ec8496
simran tomar <112646002+Simt23@users.noreply.github.com>
2022-09-12 20:38:52 +0530

    Add files via upload

---
+++ traffic_light.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:49:12 09/05/2022 
+// Design Name: 
+// Module Name:    traffic_light 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module traffic_light(
+    );
+
+
+endmodule
